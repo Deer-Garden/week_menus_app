caa4d74595ea6d98d88511a054e04497231d4b78
Deer-Garden <willkinsongs.alais@gmail.com>
2021-01-05 18:24:02 +0900

    activestrage導入

--- app/models/main_menu.rb
+++ app/models/main_menu.rb
@@ -1,2 +1,12 @@
 class MainMenu < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name 
+    validates :expiration
+    validates :kcal
+    validates :image
+    
+  end
 end
--- db/migrate/20210105064535_create_main_menus.rb
+++ db/migrate/20210105064535_create_main_menus.rb
@@ -2,6 +2,7 @@ class CreateMainMenus < ActiveRecord::Migration[6.0]
   def change
     create_table :main_menus do |t|
       t.string :name           ,null: false    ,default: ""
+      t.text :image            ,null: false    
       t.string :expiration     ,null: false    ,default: "" 
       t.string :kcal           ,null: false    ,default: ""
       t.references :user       ,null: false    ,foreign_key: true
