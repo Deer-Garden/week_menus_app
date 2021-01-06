c461f717f3d5f08e917e07620a6f091580ae399b
Deer-Garden <willkinsongs.alais@gmail.com>
2021-01-06 15:05:25 +0900

    ユーザー情報表示の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :configure_permtted_parameters, if: :devise_controller?
 
 
   private
   def configure_permtted_parameters
-    devse_parameter_sanitizer.permit(:singn_up, keys: [:nickname, :email, :password])
+    devise_parameter_sanitizer.permit(:singn_up, keys: [:nickname, :email, :password])
   end
 end
